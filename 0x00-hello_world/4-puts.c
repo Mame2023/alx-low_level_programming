9c93ccba720a3e3f13dc736d9938e8963f2279b0
Mame2023 <abumuslim131215@gmail.com>
2023-02-16 11:03:40 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
-/*
- * main - print a line of code using puts
+/**
+ * main -print a line of code using puts
  *
  * Return: 0
  */
+
 int main(void)
 {
-        put("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
